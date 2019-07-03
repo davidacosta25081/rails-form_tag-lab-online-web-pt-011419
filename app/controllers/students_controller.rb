d1d985f685f2f51e2adddbe150e1fa85ee17bde0
David Acosta <davidacosta25081@yahoo.com>
2019-07-03 17:43:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-
+before_action :set_student, only: :show
 
   def index
     @students = Student.all
@@ -16,5 +16,12 @@ class StudentsController < ApplicationController
   redirect_to students_path
 end
 
-  
+private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+
+
 end
